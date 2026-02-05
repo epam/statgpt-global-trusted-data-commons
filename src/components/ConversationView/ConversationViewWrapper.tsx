bbecdeb73c3da7bb1d58ae8958a8823dbfa155ff
Mikhail Hahalushka <Mikhail_Hahalushka@epam.com>
2026-02-05 18:24:19 +0400

    feat: migrate read-only server actions to API route handlers

--- src/components/ConversationView/ConversationViewWrapper.tsx
+++ src/components/ConversationView/ConversationViewWrapper.tsx
@@ -42,17 +42,6 @@ import {
   TimeI18nKeys,
   WelcomeI18nKeys,
 } from '../../constants/i18n-keys';
-import { getFile } from '../../app/actions/attachments';
-import { getConstraints } from '../../app/actions/constraints';
-import {
-  getConversation,
-  updateConversation,
-  createConversation,
-  getConversations,
-  rateResponse,
-} from '../../app/actions/conversations';
-import { getBucket } from '../../app/actions/bucket';
-import { getDataSet, getDataSetData } from '../../app/actions/dataset';
 import AdvancedModeIcon from '../../../public/images/advanced-mode.svg';
 import UnfoldIcon from '../../../public/images/unfold.svg';
 import DownloadIcon from '../../../public/images/chat/download.svg';
@@ -84,6 +73,17 @@ import { JWT } from 'next-auth/jwt';
 import { SIGN_IN_LINK } from '../../constants/auth';
 import { wrapWithAuthHandler } from '../../utils/auth/requests-wrapper';
 import { useLogout } from '../../hooks/use-logout';
+import { getFileApi } from '../../app/api/files/client';
+import { getBucketApi } from '../../app/api/bucket/client';
+import { getConstraintsApi } from '../../app/api/constraints/client';
+import {
+  createConversationApi,
+  getConversationApi,
+  getConversationsApi,
+  updateConversationApi,
+} from '../../app/api/conversations/client';
+import { rateResponseApi } from '../../app/api/rate/client';
+import { getDataSetApi, getDataSetDataApi } from '../../app/api/dataset/client';
 
 interface Props {
   bucketId: string;
@@ -205,11 +205,11 @@ const ConversationViewWrapper: FC<Props> = ({
   const attachmentsActions = useMemo(
     () =>
       ({
-        getFile: authHandler(getFile),
-        getDataSet: authHandler(getDataSet),
-        getDataSetData: authHandler(getDataSetData),
+        getFile: authHandler(getFileApi),
+        getDataSet: authHandler(getDataSetApi),
+        getDataSetData: authHandler(getDataSetDataApi),
         downloadDataSet: openDownloadWindow,
-        getConstraints: authHandler(getConstraints),
+        getConstraints: authHandler(getConstraintsApi),
         updateCurrentDataQuery: (dataQuery?: DataQuery) => {
           setCurrentDataQuery(dataQuery);
         },
@@ -224,12 +224,12 @@ const ConversationViewWrapper: FC<Props> = ({
   );
   const conversationViewActions = useMemo(
     () => ({
-      getConversation: authHandler(getConversation),
-      getConversations: authHandler(getConversations),
-      updateConversation: authHandler(updateConversation),
-      getBucket: authHandler(getBucket),
-      createConversation: authHandler(createConversation),
-      rateResponse: authHandler(rateResponse),
+      getConversation: authHandler(getConversationApi),
+      getConversations: authHandler(getConversationsApi),
+      updateConversation: authHandler(updateConversationApi),
+      getBucket: authHandler(getBucketApi),
+      createConversation: authHandler(createConversationApi),
+      rateResponse: authHandler(rateResponseApi),
       ...attachmentsActions,
     }),
     [attachmentsActions, authHandler],
@@ -429,7 +429,7 @@ const ConversationViewWrapper: FC<Props> = ({
             conversation,
             conversationKey,
             setConversation,
-            updateConversation: authHandler(updateConversation),
+            updateConversation: authHandler(updateConversationApi),
           }}
           attachmentsProps={{
             currentDataQuery,
--- src/constants/share-conversation.tsx
+++ src/constants/share-conversation.tsx
@@ -1,19 +1,19 @@
-import {
-  getConversation,
-  generateConversationLink,
-  getSharedConversations,
-  revokeSharedConversations,
-} from '../app/actions/conversations';
 import { IconUpload } from '@tabler/icons-react';
 import { ShareConversationProps } from '@epam/statgpt-conversation-view';
 import { AuthHandler } from '../utils/auth/requests-wrapper';
+import { getConversationApi } from '../app/api/conversations/client';
+import {
+  generateConversationLinkApi,
+  getSharedConversationsApi,
+  revokeSharedConversationsApi,
+} from '../app/api/share/client';
 
 export const SHARE_CONVERSATION_PROPS = (
   authHandler: AuthHandler,
 ): ShareConversationProps => ({
   shareIcon: <IconUpload />,
-  getConversation: authHandler(getConversation),
-  generateConversationLink: authHandler(generateConversationLink),
-  getSharedConversations: authHandler(getSharedConversations),
-  revokeSharedConversations: authHandler(revokeSharedConversations),
+  getConversation: authHandler(getConversationApi),
+  generateConversationLink: generateConversationLinkApi,
+  getSharedConversations: authHandler(getSharedConversationsApi),
+  revokeSharedConversations: authHandler(revokeSharedConversationsApi),
 });
