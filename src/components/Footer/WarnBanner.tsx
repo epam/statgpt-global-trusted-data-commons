8c1130c8c8cf90432cb3a0c277bceb9e935058b6
Mikhail Hahalushka <Mikhail_Hahalushka@epam.com>
2026-03-04 19:11:49 +0400

    feat: restyle warn-banner

--- src/components/Footer/WarnBanner.tsx
+++ src/components/Footer/WarnBanner.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 export const WarnBanner = ({ children }: { children: ReactNode }) => (
-  <div className="flex items-center justify-center w-full h-8 body-3 bg-semantic-warning-light text-neutral-1000">
+  <div className="flex items-center justify-center w-full h-10 h4 bg-semantic-warning-light text-neutral-900">
     {children}
   </div>
 );
