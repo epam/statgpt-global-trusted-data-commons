8fbe5ae6b71eec4c65da9a5ff37e4ef8c05b072f
Mikhail Hahalushka <63146655+likeamike@users.noreply.github.com>
2026-06-08 14:23:27 +0400

    feat: replace WarnBanner plain text + linkText with markdown message supporting inline mailto and #popup links (#302)

--- src/utils/banner.ts
+++ src/utils/banner.ts
@@ -6,7 +6,6 @@ export const getBannerConfig = (): BannerConfig | undefined => {
 
   return {
     message,
-    linkText: process.env.INFO_BANNER_LINK_TEXT,
     modalTitle: process.env.INFO_BANNER_MODAL_TITLE,
     modalContent: process.env.INFO_BANNER_MODAL_CONTENT,
   };
